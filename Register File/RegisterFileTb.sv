e75314b6187c0c4b6e973a0595a8c7ef8c0a7f8a
RephlexZero <rephlexzero@protonmail.com>
2023-12-06 22:21:25 +0000

    RF TB

--- Register File/RegisterFileTb.sv
+++ Register File/RegisterFileTb.sv
@@ -10,15 +10,81 @@
 // OF CODE TO TEST ALL THE FUNCTIONALITIES OF YOUR MODULE
 //
 
-module RegisterFileTb();
-	logic        Clock = 0;
-	logic [5:0]  AddressA;
-	logic [15:0] ReadDataA;
-	logic [15:0] WriteData;
-	logic        WriteEnable;
-	logic [5:0]  AddressB;
-	logic [15:0] ReadDataB;
+module RegisterFileTB;
+    // Parameters
+    parameter AddressWidth = 6;
+    parameter RegisterHeight = 1 << AddressWidth;
+    parameter RegisterWidth = 16;
 
+    // Inputs
+    logic Clock;
+    logic WriteEnable;
+    logic [RegisterWidth-1:0] WriteData;
+    logic [AddressWidth-1:0] AddressA, AddressB;
+
+    // Outputs
+    logic [RegisterWidth-1:0] ReadDataA, ReadDataB;
+
+    // Instantiate the RegisterFile module
+    RegisterFile #(
+        .AddressWidth(AddressWidth),
+        .RegisterHeight(RegisterHeight),
+        .RegisterWidth(RegisterWidth)
+    ) dut (
+        .Clock(Clock),
+        .WriteEnable(WriteEnable),
+        .WriteData(WriteData),
+        .AddressA(AddressA),
+        .AddressB(AddressB),
+        .ReadDataA(ReadDataA),
+        .ReadDataB(ReadDataB)
+    );
+
+    // Clock generation
+    always #5 Clock = ~Clock;
+
+    // Testbench stimulus
+    initial begin
+        // Initialize inputs
+        Clock = 0;
+        WriteEnable = 1;
+        WriteData = 16'hABCD;
+        AddressA = 6'b001100;
+        AddressB = 6'b010101;
+
+        // Wait for a few clock cycles
+        #10;
+
+        // Display initial register values
+        $display("Initial Register Values:");
+        for (int i = 0; i < RegisterHeight; i++) begin
+            $display("Register[%0d] = %h", i, dut.Registers[i]);
+        end
+
+        // Perform write operation
+        #10;
+        WriteEnable = 1;
+        WriteData = 16'h1234;
+        AddressA = 6'b001100;
+
+        // Wait for a few clock cycles
+        #10;
+
+        // Perform read operation
+        WriteEnable = 0;
+        AddressA = 6'b001100;
+        AddressB = 6'b010101;
+
+        // Wait for a few clock cycles
+        #10;
+
+        // Display final register values
+        $display("Final Register Values:");
+        for (int i = 0; i < RegisterHeight; i++) begin
+            $display("Register[%0d] = %h", i, dut.Registers[i]);
+        end
+
+        // End simulation
+        $finish;
+    end
 endmodule
-
-logic [RegisterWidth-1:0] Registers [Number of Registers]; 

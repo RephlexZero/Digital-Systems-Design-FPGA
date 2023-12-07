fc236dbf182d360bb426f4a8cb7ac58a2d5a2e15
RephlexZero <rephlexzero@protonmail.com>
2023-12-07 15:43:32 +0000

    VGA Working state

--- VGA Controller/VgaController.sv
+++ VGA Controller/VgaController.sv
@@ -20,6 +20,12 @@ module VgaController
 	// use this signal as counter for the vertical axis
 	logic [ 9:0] vCount;
 
+	// Initialize counters
+    initial begin
+        hCount = 0;
+        vCount = 0;
+    end
+
 	// VGA timing parameters
 	parameter H_DISPLAY = 800;
 	parameter H_FRONT_PORCH = 56;
@@ -32,44 +38,36 @@ module VgaController
 	parameter V_BACK_PORCH = 23;
 
 
-	always_ff @(posedge Clock) begin
+	always_ff @(posedge Clock or posedge Reset) begin
 		if (Reset) begin
 			hCount <= 0;
 			vCount <= 0;
 		end else begin
 			// If we are at the end of the row, reset the horizontal counter and increment the vertical counter
-			if (hCount == H_DISPLAY + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH) begin
+			if (hCount > H_DISPLAY + H_FRONT_PORCH + H_SYNC_PULSE + H_BACK_PORCH - 1) begin
 				hCount <= 0;
-				vCount <= vCount + 1;
-			// If we are at the end of the screen, reset the vertical counter
-			if (vCount == V_DISPLAY + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH) begin
-				vCount <= 0;
-			end
-			// Otherwise, increment the horizontal counter
+				// If we are at the end of the screen, reset the vertical counter
+				if (vCount > V_DISPLAY + V_FRONT_PORCH + V_SYNC_PULSE + V_BACK_PORCH - 1) begin
+					vCount <= 0;
+				end else begin
+					vCount <= vCount + 1;
+				end
 			end else begin
 				hCount <= hCount + 1;
 			end
 		end
 	end
 
-	// Generate sync and blank signals
-	assign hSync_n = (hCount < H_DISPLAY + H_FRONT_PORCH) && (hCount >= H_DISPLAY + H_FRONT_PORCH + H_SYNC_PULSE);
-	assign vSync_n = (vCount < V_DISPLAY + V_FRONT_PORCH) && (vCount >= V_DISPLAY + V_FRONT_PORCH + V_SYNC_PULSE);
-	assign sync_n = ~hSync_n || ~vSync_n;
-	assign blank_n = (vCount < V_DISPLAY) && (hCount < H_DISPLAY);
-
-	// Output next X and Y pixel positions if not in blanking period
 	always_comb begin
-		if (vCount < V_DISPLAY) begin
-			nextY = vCount;
-		end else begin
-			nextY = 0;
-		end
-		if (hCount < H_DISPLAY) begin
-			nextX = hCount;
-		end else begin
-			nextX = 0;
-		end
+		// Active low sync signals
+		hSync_n = (hCount < H_DISPLAY + H_FRONT_PORCH) || (hCount >= H_DISPLAY + H_FRONT_PORCH + H_SYNC_PULSE);
+		vSync_n = (vCount < V_DISPLAY + V_FRONT_PORCH) || (vCount >= V_DISPLAY + V_FRONT_PORCH + V_SYNC_PULSE);
+		sync_n = ~hSync_n || ~vSync_n;
+		blank_n = (vCount < V_DISPLAY) && (hCount < H_DISPLAY);
+
+		// Output next X and Y pixel positions if not in blanking period
+		nextX = (hCount < H_DISPLAY) ? hCount : 0;
+		nextY = (vCount < V_DISPLAY) ? vCount : 0;
 	end
 
 endmodule

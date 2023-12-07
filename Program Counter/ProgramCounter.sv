f95fe2754db4179486d5268b1d65663aeec54d4e
RephlexZero <rephlexzero@protonmail.com>
2023-12-07 02:16:58 +0000

    Fix signing in PC

--- Program Counter/ProgramCounter.sv
+++ Program Counter/ProgramCounter.sv
@@ -1,8 +1,8 @@
 module ProgramCounter (
     input logic Clock, Reset, LoadEnable, OffsetEnable,
-    input logic [15:0] LoadValue,
-    input logic [8:0] Offset,
-    output logic [15:0] CounterValue
+    input logic signed [15:0] LoadValue,
+    input logic signed [8:0] Offset,
+    output logic signed [15:0] CounterValue
 );
 
     logic [15:0] internalCounterValue;
@@ -18,9 +18,9 @@ module ProgramCounter (
             // Check sign bit for negative Offset, decide if add or subtract.
             if (Offset[8])
                 // Length of internalCounterValue is 16 bits, so we need to pad Offset with 7 zeros.
-                internalCounterValue <= internalCounterValue - {{7{1'b0}}, Offset};
+                internalCounterValue <= internalCounterValue - Offset;
             else
-                internalCounterValue <= internalCounterValue + {{7{1'b0}}, Offset};
+                internalCounterValue <= internalCounterValue + Offset;
         end
         else
             internalCounterValue <= internalCounterValue + 1;
